250241f1eccfabed190cad28842ca28a92c2249c
pancholivaibhav1995 <pancholivaibhav@gmail.com>
2025-05-16 22:28:12 +0530

    Add files via upload
    admin page changes

--- admin/js/dashboard-script.js
+++ admin/js/dashboard-script.js
@@ -2,4 +2,8 @@ document.getElementById("toggleSidebar").addEventListener("click", () => {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
   });
+  document.getElementById("toggleSidebar").addEventListener("click", () => {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("show");
+  });
   
